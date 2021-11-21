d832fa365c91a6a1592db5e5a8654a63d65933f2
dbhardman <dave.h1730@gmail.com>
2021-11-20 16:48:41 -0800

    Add icon file and update the config and run.sh files after testing

---
+++ backup_to_usb/README.md
@@ -0,0 +1,21 @@
+The title is a little misleading as the addon only copies existing
+backup files to an external drive using the folders available in Hassio
+
+Based on work by NotoriusBDG   https://community.home-assistant.io/t/snapshot-service-enhancement-to-usb-dongle/43202/2
+
+Prior to installing this addon you need a way to mount the usb drive.
+The best method I found was this:  https://gist.github.com/eklex/c5fac345de5be9d9bc420510617c86b5
+This process survives reboots and upgrades.   Follow the directions in the post above then reboot the Host from the supervisor/system in home assistant.
+
+To install the addon.  Copy the url from Code button.  In home Assistant, select Supervisor/Add-on Store.   Select options (the three dots in the upper right corner) and select Repositories.  and paste the Github url
+and add the repositiory.   Next select the options and select Reload.  The addon should now appear in the store listings.
+
+Select addon and click install.
+
+Once complete, select the configuration tab and ensure the path is set as per the path you used in the rules file.   Set the number backups you want to keep internally and externally and save the configuration.
+
+The addon does not have to be started, as it simply runs through and stops.   The best way to use in an automation is to call Service, select hassio.addon_start and select the addon slug in the drop down box.
+
+You can call from the Developer Tools to test.
+
+
--- backup_to_usb/run.sh
+++ backup_to_usb/run.sh
@@ -11,20 +11,36 @@ echo "[INFO] MAX_LOCAL=$MAX_LOCAL"
 echo "[INFO] MAX_REMOTE=$MAX_REMOTE"
 echo "[INFO] BACKUP_PATH=$BACKUP_PATH"
 
-echo "[INFO] Mounting /dev/sda1 to /mnt"
-mount /dev/sda1 /mnt
-
-# Create /mnt/$BACKUP_PATH folder
-if [ ! -d /mnt/$BACKUP_PATH ]; then
-  echo "[INFO] Creating /mnt/$BACKUP_PATH"
-  mkdir -p /mnt/$BACKUP_PATH
+# test the base directory to ensure a path into the container
+# fail otherwise
+BASE_DIRECTORY=$(echo "$BACKUP_PATH" | cut -d "/" -f1)
+echo "[INFO] Base directory is $BASE_DIRECTORY"
+if [ ! -d /$BASE_DIRECTORY ]; then
+  echo "[INFO] $BASE_DIRECTORY is not mapped to container, copy will fail"
+  echo "[INFO] Showing available folders mapped to container"
+  mount
+  exit 1
+else
+  echo "[INFO] $BASE_DIRECTORY is mapped to container"
 fi
 
+#Create /$BACKUP_PATH folder if it does not exist
+if [ ! -d /$BACKUP_PATH ]; then
+  echo "[INFO] Creating /$BACKUP_PATH"
+  mkdir -p -v /$BACKUP_PATH
+else
+  echo "[INFO] $BACKUP_PATH exists"  
+fi
+
+# use rsync to copy over any backups from the sd card backup folder to $BACKUP_PATH
+# -v (verbose) -t (preserve time stamp)
 echo "[INFO] Copying all backups from /backup to /mnt/$BACKUP_PATH"
 rsync -v /backup/*.tar /mnt/$BACKUP_PATH
 
+#cleanup backup folder only keeping $MAX_LOCAL (default 2 ) images
 echo "[INFO] Purging backups from /backup except for the last $MAX_LOCAL"
 ls -tp /backup/*.tar | grep -v '/$' | tail -n +$MAX_LOCAL_PLUS1 | xargs -I {} rm -- "{}"
 
+# cleanup the $BACKUP_PATH only keeping $MAX_REMOTE (default 7 ) images
 echo "[INFO] Purging backups from /mnt/$BACKUP_PATH except for the last $MAX_REMOTE"
 ls -tp /mnt/$BACKUP_PATH/*.tar | grep -v '/$' | tail -n +$MAX_REMOTE_PLUS1 | xargs -I {} rm -- "{}"
